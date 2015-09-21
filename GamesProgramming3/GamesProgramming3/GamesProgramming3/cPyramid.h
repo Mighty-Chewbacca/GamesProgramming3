9a5708690f5574707f6d943473f8eb3b3ed4debd
Alex Malcolm <alexmalcolm93@live.co.uk>
2015-09-21 12:50:57 +0000

    initial commit
    initial commit

---
+++ GamesProgramming3/GamesProgramming3/GamesProgramming3/cPyramid.h
@@ -0,0 +1,38 @@
+#ifndef _PYRAMID_H
+#define _PYRAMID_H
+
+#include "windowOGL.h"
+#include "glm\glm.hpp"
+#include "glx\glext.h"
+#include "wglext.h"
+#include <vector>
+#include "cColours.h"
+#include "cShapes.h"
+
+class cPyramid : public cShapes
+{
+public:
+	cPyramid();
+	virtual void render(float rotAngle);
+	virtual void initialise();
+
+private:
+
+	glm::vec3 m_topLeftFront;
+	glm::vec3 m_bottomLeftFront;
+	glm::vec3 m_topRightFront;
+	glm::vec3 m_bottomRightFront;
+	glm::vec3 m_topLeftBack;
+	glm::vec3 m_topRightBack;
+	glm::vec3 m_bottomLeftBack;
+	glm::vec3 m_bottomRightBack;
+
+	glm::vec3 m_Faces[24];
+	GLuint m_Indices[36];
+
+	std::vector<glm::vec3> m_vertices;
+	std::vector<glm::vec3> m_colourList;
+	GLuint m_VertexBufferObjects[3];
+};
+
+#endif
